79530073c1afc425f3ecd81af6487fd9a2f467e4
Bruno Mota <bmotadev@gmail.com>
2023-03-06 14:34:49 -0300

    Testes com mock e HTTP

--- test/meu_app_testes_test.dart
+++ test/meu_app_testes_test.dart
@@ -1,8 +1,14 @@
 import 'dart:js_util';
 
+import 'package:meu_app_testes/classes/viacep.dart';
 import 'package:meu_app_testes/meu_app_testes.dart' as app;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'meu_app_testes_test.mocks.dart';
+
+@GenerateMocks([MockViaCEP]) //anotation
 void main() {
   test('Calcula o valor do produto com desconto e sem porcentagem', () {
     expect(app.calcularDesconto(1000, 150, false), equals(850));
@@ -77,4 +83,28 @@ void main() {
   test('Valor diferente', () {
     expect(app.retornaValor(50), isNot(equals(49)));
   });
+
+  test('Retornar CEP', () async {
+    MockMockViaCEP mockMockViaCEP = new MockMockViaCEP();
+    when(mockMockViaCEP.retornarCEP("01001000"))
+        .thenAnswer((realInvocation) => Future.value({
+              "cep": "01001-000",
+              "logradouro": "Praça da Sé",
+              "complemento": "lado ímpar",
+              "bairro": "Sé",
+              "localidade": "São Paulo",
+              "uf": "SP",
+              "ibge": "3550308",
+              "gia": "1004",
+              "ddd": "11",
+              "siafi": "7107"
+            }));
+
+    var body = await mockMockViaCEP.retornarCEP("01001000");
+    print(body);
+    expect(body["bairro"], equals("Sé"));
+    expect(body["logradouro"], equals("Praça da Sé"));
+  });
 }
+
+class MockViaCEP extends Mock implements ViaCEP {}
